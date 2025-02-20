3d2b8624e46ecb04aeedf169841ab4ebace8c1c6
Devarsh01 <91387390+Devarsh01@users.noreply.github.com>
2025-02-20 12:36:53 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,15 +6,16 @@ from model_utils import ModelEvaluator, ModelSaver
 
 def main():
     file_path = "/home/ubuntu/dev/Data/final_data.parquet"
-    data = pd.read_parquet(file_path, engine="pyarrow").sample(frac=1, random_state=42).head(1000000)
+    data = pd.read_parquet(file_path, engine="pyarrow").sample(frac=1, random_state=42)
     train_df, test_df = DataSplitter.train_test_split_by_field(data)
     preprocessor = DataPreprocessor()
     X_train, y_train, le, feature_cols = preprocessor.prepare_data(train_df)
     X_test, y_test, _, _ = preprocessor.prepare_data(test_df)
     models = {
         'Logistic Regression': LogisticRegressionModel(),
-        'Random Forest': RandomForestModel(),
-        'LightGBM': LightGBMModel()
+        'Random Forest': RandomForestModel()
+        'LightGBM': LightGBMModel(),
+        'XGBoost': XGBoostModel()
     }
     evaluator = ModelEvaluator()
     results, scaler = evaluator.train_and_evaluate(models, X_train, X_test, y_train, y_test, le)
